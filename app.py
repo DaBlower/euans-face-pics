ed3be654bfd5203a5495e90cdb76ac62bf3344d9
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-10-14 16:30:05 +1100

    mailto

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, send_file
 import os
+from dotenv import load_dotenv
 import random
 
 app = Flask(__name__)
@@ -8,14 +9,22 @@ project_root = os.path.dirname(__file__)
 
 IMAGE_FOLDER = os.path.join(project_root, "images")
 
+load_dotenv()
+
+os.makedirs(IMAGE_FOLDER, exist_ok=True)
+
 @app.route("/random_euan")
 def random_euan():
     images = [f for f in os.listdir(IMAGE_FOLDER)
              if os.path.isfile(os.path.join(IMAGE_FOLDER, f))]
     
     if not images:
-        return "sorry, there aren't any images (aka something went horribly wrong), please contact obob@duck.com", 404
+        mail = os.getenv("dev_email")
+        return f"""sorry, there aren't any images (aka something went horribly wrong), please contact <a href="mailto:{mail}">{mail}</a>""", 404
     
     chosen = random.choice(images)
 
     return send_file(os.path.join(IMAGE_FOLDER, chosen), mimetype="image/*")
+
+if __name__ == "__main__":
+    app.run()
